649f44bae7efc833d6e7ae8e10b6b69c61a99685
Justin Solo <justinpaulosolo@gmail.com>
2022-11-20 23:42:07 -0800

    Neovim config

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -57,19 +57,18 @@ vim.api.nvim_create_autocmd("LspAttach", {
     bufmap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
 
     -- Renames all references to the symbol under the cursor
-    bufmap("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>")
+    bufmap("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<cr>")
 
     -- Selects a code action available at the current cursor position
-    bufmap("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>")
-    bufmap("x", "<F4>", "<cmd>lua vim.lsp.buf.range_code_action()<cr>")
+    bufmap("n", "ca", "<cmd>lua vim.lsp.buf.code_action()<cr>")
 
     -- Show diagnostics in a floating window
-    bufmap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
+    bufmap("n", "<leader>dl", "<cmd>lua vim.diagnostic.open_float()<cr>")
 
     -- Move to the previous diagnostic
-    bufmap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+    bufmap("n", "<leader>dp", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
 
     -- Move to the next diagnostic
-    bufmap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+    bufmap("n", "<leader>dn", "<cmd>lua vim.diagnostic.goto_next()<cr>")
   end,
 })
